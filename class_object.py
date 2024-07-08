05d4fb1ec4f9159c86d12086efd27f410747ef79
harshi-07 <harshithasridhar27@gmail.com>
2024-07-08 15:54:09 +0530

    new file

---
+++ class_object.py
@@ -0,0 +1,19 @@
+class myclass:
+    def myfunc(self):
+        pass
+    def display(selfself,name):
+        print("my name is:",name)
+mc=myclass()
+mc.myfunc()
+mc.display("harshi")
+
+
+#instance method
+
+class myclass:
+    def m1(self):
+        print("this is method 1")
+a=myclass()
+a.m1()
+
+
