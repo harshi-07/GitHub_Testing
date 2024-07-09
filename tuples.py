54fa2a9fd23273ba697f5a7cd466936ce46f7a9f
harshi-07 <harshithasridhar27@gmail.com>
2024-07-09 11:10:48 +0530

    this is second file

---
+++ tuples.py
@@ -0,0 +1,15 @@
+a=(1,2,3,4)
+print(a)
+a=tuple("abc")
+print(a)
+a=(1,2,3,4,5,6)
+print(len(a))
+print(min(a))
+print(max(a))
+
+a=(1,2,3,4,5)
+for i in a:
+    print(i,end=" ")
+
+a=(1,2,3,4,5)
+print(a[0:3])
